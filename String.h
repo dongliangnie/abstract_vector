99da1ff55b463ebedbe3f2ed880aea7318551043
paidaxing-org <fdfx_zhaowenyu@126.com>
2024-10-25 20:23:13 +0800

    Update String.h

--- String.h
+++ String.h
@@ -11,8 +11,10 @@ public:
     void input(istream& in);
     String operator+(String& str);
     int size();
-    friend ostream& operator<<(ostream& out,String& str);
+    friend ostream& operator<<(ostream& out, String& str);
     friend istream& operator>>(istream& in, String& str);
+    bool operator==(const String& s) const;
+    bool operator==(const char* s) const;
 };
 void ACauto();
 void Manacher();
