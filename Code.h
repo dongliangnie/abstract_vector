24aad298364097dbc928ffc5a01ca0572d927a08
paidaxing-org <fdfx_zhaowenyu@126.com>
2024-10-27 10:31:03 +0800

    修改了test.cpp的注释

--- Code.h
+++ Code.h
@@ -8,8 +8,8 @@ extern int CodeNum2;                               //栅栏密码中字符串长
 using namespace std;
 void Code(String PreStr, int CodeNum1);            //对输入字符串进行加密
 String EnCode(int CodeNum1);                       //对文档中字符串进行解密
-bool JudgeCode(String& Strin);                     //判断输入字符串与文档中字符串是否相同，相同则输出1，不同则输出0
-void Save(String& LatStr);                         //保存字符串到文档中
+bool JudgeCode(String& Strin);                     //判断输入字符串与文档中字符串是否相同，相同则返回1，不同则返回0
+void Save(String& LatStr);                         //保存加密后字符串到文档中
 String Load();                                     //返回从文档中读取的加密后字符串
 void JudgeisCorrect(String Strin);                 //判断输入的字符串与文档中字符串解密后是否相同            
 void changeCode(String& Strin);                    //更改字符串，如果与原字符串不同，对其进行加密并存到文档中；如果相同，则返回
